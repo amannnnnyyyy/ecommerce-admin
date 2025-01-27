3204f2f483386c342b5a67752b671d6118f8d4b6
Amanuel Moha <mohaaman922@gmail.com>
2025-01-27 15:56:57 +0300

    title and button

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+   <section>
+    This is Admin Dashboard  <Button>Click here</Button>
+   </section>
+  );
+}
