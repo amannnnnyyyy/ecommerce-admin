0900ac7e3ea68a6d6bd580c292ce13cc434a7198
Amanuel Moha <mohaaman922@gmail.com>
2025-01-30 14:31:20 +0300

    pushing store table to db

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ModalProvider } from "@/providers/modal-provider";
-import prismadb from "@/lib/prismadb";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
