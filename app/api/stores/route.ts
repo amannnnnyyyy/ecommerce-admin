42f1d39679afb221de1be0316d4ec22c090ef46e
Amanuel Moha <mohaaman922@gmail.com>
2025-01-30 16:07:01 +0300

    form correction

--- app/api/stores/route.ts
+++ app/api/stores/route.ts
@@ -1,3 +1,4 @@
+import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
@@ -11,6 +12,13 @@ export async function POST(req: Request){
         if(!userId) return new NextResponse("Unauthorized",{status: 401})
         if(!name) return new NextResponse("Name is required",{status:400})
 
+            const store = await prismadb.store.create({
+                data: {
+                    name,
+                    userId
+                }
+            });
+            return NextResponse.json(store,{status:201})
     } catch (error) {
         console.log('[STORES_POST]',error);
         return new NextResponse("Internal Server Error",{status:500})
--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -7,6 +7,7 @@ import * as z from "zod"
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { Button } from "../ui/button"
+import { useState } from "react"
 
 const formSchema = z.object({
         name: z.string().nonempty("Name is required!").min(2,"Name should be more than 1 character"),
@@ -14,6 +15,8 @@ const formSchema = z.object({
 export const StoreModal = () =>{
     const storeModal = useStoreModal()
     
+    const [loading, setLoading] = useState(false)
+
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues:{
@@ -22,14 +25,34 @@ export const StoreModal = () =>{
     });
 
     const onSubmit = async(values:z.infer<typeof formSchema>)=>{
-        console.log(values)
+        try{
+            setLoading(true)
+
+            // const response = await axios
+        }catch(error){
+            console.log(error)
+        }finally{
+            setLoading(false)
+        }
     }
 
   return(  
     <Modal title="Create Store"
-        description="You must add a new store before continuing"
+        description="Enter your store name to create a new store"
         isOpen={storeModal.isOpen}
         onClose={storeModal.onClose}>
+           <div className="flex flex-col md:flex-row">
+                <img className="top-0 h-36 w-36 self-center" src="emptyStore.png" alt="" />
+                <div>
+                <h1 className="text-2xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-purple-600 text-center drop-shadow-lg">
+                    You have no store 🛒🚫
+                </h1>
+
+                <p className="mt-4 text-lg text-gray-500 text-center px-6 max-w-2xl mx-auto leading-relaxed">
+                    You must have atleast one store to continue
+                </p>
+                </div>
+           </div>
             <div>
                 <div>
                     <Form {...form}>
@@ -40,7 +63,7 @@ export const StoreModal = () =>{
                                         <FormItem>
                                             <FormLabel> Name </FormLabel>
                                             <FormControl>
-                                                <input className="ml-5 w-full border-2 border-opacity-30 border-slate-500" placeholder="E-commerce" {...field}/>
+                                                <input disabled={loading} className="ml-5 w-full border-2 border-opacity-30 border-slate-500" placeholder="E-commerce" {...field}/>
                                             </FormControl>
                                             <FormMessage/>
                                         </FormItem>
@@ -49,8 +72,8 @@ export const StoreModal = () =>{
 
                             </FormField>
                            <div className="flex gap-5">
-                                <Button type="button" className="bg-secondary bottom-0 self-center w-2/3 hover:bg-red-800 text-black hover:text-white" onSubmit={storeModal.onClose}>Cancel</Button>
-                                <Button type="submit" className="bottom-0 self-center w-2/3 hover:bg-green-800">Create</Button>
+                                <Button disabled={loading} type="button" className="bg-secondary bottom-0 self-center w-2/3 hover:bg-red-800 text-black hover:text-white" onSubmit={storeModal.onClose}>Cancel</Button>
+                                <Button disabled={loading} type="submit" className="bottom-0 self-center w-2/3 hover:bg-green-800">Create</Button>
                             </div>
                         </form>
                     </Form>
