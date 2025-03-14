a7b8ebb344ee2652b0adf8b42ab44c83c6b0e46a
Amanuel Moha <mohaaman922@gmail.com>
2025-03-14 15:56:50 +0300

    dashboard

--- app/api/stores/route.ts
+++ app/api/stores/route.ts
@@ -2,9 +2,7 @@ import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-export async function POST(req: Request){
-    console.log("hhhhhhhherreeee we are")
-    
+export async function POST(req: Request){    
     try {
         const body = await req.json();
 
