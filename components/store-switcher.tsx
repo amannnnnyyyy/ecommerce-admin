8e8c4fa6d786069cff35b02c6a6f07707859fbe2
Amanuel Moha <mohaaman922@gmail.com>
2025-03-17 10:40:38 +0300

    store switcher added

--- components/store-switcher.tsx
+++ components/store-switcher.tsx
@@ -1,3 +1,56 @@
-export default function StoreSwitcher(){
-    return <div>Store switcher</div>
+"use client"
+
+import { Popover, PopoverTrigger } from "@/components/ui/popover"
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { Store } from "@prisma/client"
+import { useParams, useRouter } from "next/navigation";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Store as StoreIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+type popoverTriggerProps = React.ComponentPropsWithoutRef<typeof PopoverTrigger>
+
+interface StoreSwitcherProps extends popoverTriggerProps{
+    items:Store[];
 }
+
+
+export default function StoreSwitcher({
+    className,
+    items = []
+}:StoreSwitcherProps){
+
+    const storeModal = useStoreModal();
+    const params = useParams();
+    const router = useRouter();
+
+    const formattedItems = items.map((item)=>({
+        label: item.name,
+        value: item.id
+    }))
+
+    const currentStore = formattedItems.find((item)=>item.value=== params.storeId)
+
+
+    const [open,setOpen]=useState(false);
+    const onStoreSelect = (store:{value:string, label:string})=>{
+        setOpen(false)
+        router.push(`/${store.value}`)
+    }
+    return (
+        <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+                <Button 
+                    variant={"outline"} 
+                    size={"sm"} 
+                    role="combobox" 
+                    aria-expanded={open}
+                    aria-label="Select a store"
+                    className={cn("w-[200px] justify-between",className)}>
+                        <StoreIcon className="mr-2 h-4 w-4"/>
+                        Current Store
+                </Button>
+            </PopoverTrigger>
+        </Popover>
+)}
