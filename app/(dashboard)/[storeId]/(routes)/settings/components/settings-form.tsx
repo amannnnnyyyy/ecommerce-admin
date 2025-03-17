8e4b5bbfe52b223423b6be03b1b723a958f5af75
Amanuel Moha <mohaaman922@gmail.com>
2025-03-17 11:36:23 +0300

    store form

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,6 +1,19 @@
-const SettingsForm = () => {
+"use client"
+
+import Heading from "@/components/ui/heading";
+import { Store } from "@prisma/client"
+
+interface SettingsFormProps{
+    initialData: Store;
+}
+const SettingsForm:React.FC<SettingsFormProps> = ({initialData}) => {
   return (
-    <div>SettingsForm</div>
+    <div className="flex items-center justify-between">
+        <Heading
+            title="settings"
+            description="Manage store preferences"
+            />
+    </div>
   )
 }
 
