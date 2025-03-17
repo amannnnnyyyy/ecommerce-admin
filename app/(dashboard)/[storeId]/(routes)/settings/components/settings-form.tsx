e97d3d3057f0ea6aa83431ddd998e264bc1926b5
Amanuel Moha <mohaaman922@gmail.com>
2025-03-17 11:58:40 +0300

    form -setting

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -1,19 +1,61 @@
 "use client"
 
-import Heading from "@/components/ui/heading";
+//global imports
+import * as z from "zod"
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Store } from "@prisma/client"
+import { Trash } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+
+//local imports
+import { Button } from "@/components/ui/button";
+import Heading from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+
 
 interface SettingsFormProps{
     initialData: Store;
 }
+
+const formSchema = z.object({
+    name: z.string().min(1)
+})
+
+type SettingsFormValues = z.infer<typeof formSchema>;
+
 const SettingsForm:React.FC<SettingsFormProps> = ({initialData}) => {
+
+    const [open,setOpen]=useState(false)
+    const [loading,setLoading] = useState(false)
+
+    const form = useForm<SettingsFormValues>({
+        resolver: zodResolver(formSchema),
+        defaultValues:initialData
+    })
+
+    const onSubmit = async (data:SettingsFormValues)=>{
+        console.log("submitted",data)
+    }
+
+    
   return (
-    <div className="flex items-center justify-between">
-        <Heading
-            title="settings"
-            description="Manage store preferences"
-            />
-    </div>
+    <>
+        <div className="flex items-center justify-between">
+            <Heading
+                title="settings"
+                description="Manage store preferences"
+                />
+                <Button
+                    variant="destructive"
+                    size="icon"
+                    onClick={()=>{}}
+                    >
+                        <Trash className="h-4 w-4"/>
+                </Button>
+        </div>
+        <Separator className="my-4"/>
+    </>
   )
 }
 
