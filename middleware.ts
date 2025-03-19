7bb1905ada7e4611a99fcdce6f328987ed1d0ed1
Amanuel Moha <mohaaman922@gmail.com>
2025-03-19 15:26:56 +0300

    make public urls public

--- middleware.ts
+++ middleware.ts
@@ -2,11 +2,32 @@ import { NextResponse } from 'next/server';
 import { auth, clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 const isPublicRoute = createRouteMatcher(['/sign-in(.*)','/sign-up(.*)']);
+const LOCAL_URL = 'http://localhost:3000';
 
 export default clerkMiddleware((auth,req)=>{
   if(!isPublicRoute(req)){
     auth.protect()
   }
+
+  const { pathname } = req.nextUrl;
+
+  if (pathname.startsWith('/api/')) {
+    return NextResponse.next();
+  }
+
+  if (!isPublicRoute(req)) {
+    auth.protect();
+  }
+
+    if (process.env.NODE_ENV === 'development') {
+        if (pathname === '/sign-up' && req.url.startsWith('https://normal-pipefish-33.accounts.dev')) {
+            return NextResponse.redirect(new URL('/sign-up', LOCAL_URL));
+        }
+    }
+
+    // Return a response if no redirect is needed
+    return NextResponse.next();
+
 });
 
 export const config = {
