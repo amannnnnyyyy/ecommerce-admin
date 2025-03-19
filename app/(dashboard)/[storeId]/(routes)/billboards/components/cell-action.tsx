0435759c1f5d0c2c35ea9e4da29701ca0178a551
Amanuel Moha <mohaaman922@gmail.com>
2025-03-19 12:19:39 +0300

    actions cell

--- app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/cell-action.tsx
@@ -1,5 +1,22 @@
-export const CellAction =() =>{
+"use client"
+
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { BillboardColumn } from "./columns"
+
+interface CellActionProps{
+    data:BillboardColumn;
+}
+
+export const CellAction:React.FC<CellActionProps> =({data}) =>{
     return(
-        <div>Action</div>
+       <DropdownMenu>
+        <DropdownMenuTrigger className="bg-blue-300 border-spacing-4 border-gray-400 rounded-lg p-1 px-3">
+            <span className="text-black">Actions</span>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+            <DropdownMenuItem>Edit</DropdownMenuItem>
+            <DropdownMenuItem>Delete</DropdownMenuItem>
+        </DropdownMenuContent>
+       </DropdownMenu>
     )
 }
