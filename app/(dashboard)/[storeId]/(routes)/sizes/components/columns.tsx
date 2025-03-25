81eff697d8e7c35cb663f229d1349e52bb8e9c99
Amanuel Moha <mohaaman922@gmail.com>
2025-03-25 11:39:07 +0300

    sizes

---
+++ app/(dashboard)/[storeId]/(routes)/sizes/components/columns.tsx
@@ -0,0 +1,50 @@
+"use client"
+
+//global imports
+import { ColumnDef } from "@tanstack/react-table"
+import { ArrowUpDown } from "lucide-react"
+
+//loacl imports
+import { Button } from "@/components/ui/button"
+import { CellAction } from "./cell-action"
+
+export type BillboardColumn = {
+  id: string
+  label: string
+  createdAt: string
+}
+
+export const columns: ColumnDef<BillboardColumn>[] = [
+  {
+    accessorKey: "label",
+    header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Label
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        )
+      },
+  },
+  {
+    accessorKey: "createdAt",
+    header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Created At
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        )
+      },
+  },
+  {
+    id: "actions",
+    cell:({row})=><CellAction data={row.original}/>
+  },
+]
