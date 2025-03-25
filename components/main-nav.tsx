865a73923a25f65b75a8be41a30f37d6225d6982
Amanuel Moha <mohaaman922@gmail.com>
2025-03-25 14:28:43 +0300

    api routes for sizes and nav links

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -19,22 +19,22 @@ export function MainNav({
     {
         href:`/${params.storeId}/billboards`,
         label:'Billboards',
-        active: pathName === `/${params.storeId}/billboards`
+        active: pathName.includes(`/${params.storeId}/billboards`)
     },
     {
         href:`/${params.storeId}/categories`,
         label:'Categories',
-        active: pathName === `/${params.storeId}/categories`
+        active: pathName.includes(`/${params.storeId}/categories`)
     },
     {
         href:`/${params.storeId}/sizes`,
         label:'Sizes',
-        active: pathName === `/${params.storeId}/sizes`
+        active: pathName.includes(`/${params.storeId}/sizes`)
     },
     {
         href:`/${params.storeId}/settings`,
         label:'Settings',
-        active: pathName === `/${params.storeId}/settings`
+        active: pathName.includes(`/${params.storeId}/settings`)
     }
 ]
     return (
