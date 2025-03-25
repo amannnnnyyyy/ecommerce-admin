42e9ee96364de7084481432f5e8e1edf8fb27d1a
Amanuel Moha <mohaaman922@gmail.com>
2025-03-25 12:03:53 +0300

    created table

--- app/(dashboard)/[storeId]/(routes)/sizes/components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/sizes/components/columns.tsx
@@ -8,22 +8,37 @@ import { ArrowUpDown } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { CellAction } from "./cell-action"
 
-export type BillboardColumn = {
+export type SizeColumn = {
   id: string
-  label: string
+  name: string
+  value: string
   createdAt: string
 }
 
-export const columns: ColumnDef<BillboardColumn>[] = [
+export const columns: ColumnDef<SizeColumn>[] = [
   {
-    accessorKey: "label",
+    accessorKey: "name",
     header: ({ column }) => {
         return (
           <Button
             variant="ghost"
             onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
           >
-            Label
+            Name
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        )
+      },
+  },
+  {
+    accessorKey: "value",
+    header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Value
             <ArrowUpDown className="ml-2 h-4 w-4" />
           </Button>
         )
--- app/(dashboard)/[storeId]/(routes)/sizes/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/sizes/page.tsx
@@ -2,14 +2,14 @@ import prismadb from "@/lib/prismadb"
 import {format} from 'date-fns'
 
 import BillboardClient from "./components/client"
-import { BillboardColumn } from "./components/columns"
+import { SizeColumn } from "./components/columns"
 
-const BillboardsPage = async({
+const SizesPage = async({
   params
 }:{
   params:{storeId:string}
 }) => {
-  const billboards = await prismadb.billboard.findMany({
+  const sizes = await prismadb.size.findMany({
     where:{
       storeId: params.storeId
     },
@@ -18,10 +18,11 @@ const BillboardsPage = async({
     }
   })
 
-  const formattedBillboards: BillboardColumn[] = billboards.map((item)=>(
+  const formattedSizes: SizeColumn[] = sizes.map((item)=>(
     {
       id: item.id,
-      label: item.label,
+      name: item.name,
+      value: item.value,
       createdAt: format(item.createdAt,"MMMM do,yyyy"),
     }
   ))
@@ -29,10 +30,10 @@ const BillboardsPage = async({
   return (
     <div className="flex flex-col">
         <div className="flex-1 space-y-4 p-8 pt-6">
-            <BillboardClient data={formattedBillboards}/>
+            <BillboardClient data={formattedSizes}/>
         </div>
     </div>
   )
 }
 
-export default BillboardsPage
+export default SizesPage
